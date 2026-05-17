0fab19a32e6267c55628d1b0fba993834503506c
Katarina Vujovic <v.katar551@gmail.com>
2026-05-17 14:27:37 +0200

    Testing workflow..

--- src/main/java/intens/api/praksa2022/controller/DemoController.java
+++ src/main/java/intens/api/praksa2022/controller/DemoController.java
@@ -9,6 +9,6 @@ public class DemoController {
     @RequestMapping("/")
     public ResponseEntity<String> getRootDemo() {
 
-        return ResponseEntity.ok().body("INTENS Praksa 2026!");
+        return ResponseEntity.ok().body("INTENS PRAKSA 2026!");
     }
 }
